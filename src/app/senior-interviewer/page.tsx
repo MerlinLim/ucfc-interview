cea395f55e4087c253845b2fb58a83b9817e7d7e
Merlin <merlinlim98@gmail.com>
2025-07-04 10:55:30 +0800

    fix bugs and change metadata

--- src/app/senior-interviewer/page.tsx
+++ src/app/senior-interviewer/page.tsx
@@ -21,8 +21,8 @@ export default function Questionnaire2() {
   if (!data) return <p>Loading...</p>;
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100 no-print">
-      <div className="max-w-4xl mx-auto p-6 text-sm md:text-base bg-white shadow-md rounded-lg print-container">
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <div className="max-w-4xl mx-auto p-6 text-sm md:text-base bg-white shadow-md rounded-lg">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Find a junior to interview!</h1>
           <button
